7305b05da8e24acb5e5f0d65ef4960a872014b24
jeremybreant <breant.jeremy@yahoo.com>
2023-03-24 12:49:22 +0100

    liste ingredient : controller + twig; install knp_paginator

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,6 +15,6 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('pages/home.html.twig');
     }
 }
