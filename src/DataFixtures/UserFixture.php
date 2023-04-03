009bb448c1bed175ff712893c8d380e3deacea2e
jeremybreant <breant.jeremy@yahoo.com>
2023-04-03 15:37:47 +0200

    user entity created + migration + fixture + entity listener to hash password everytime

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixture extends Fixture
+{
+    /**
+     * @var Generator
+     */
+    private Generator $faker;
+
+    public function  __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 10; $i++){
+
+            $user = new User();
+
+            $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0,1) === 1 ? $this->faker->firstName() : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('pasword');
+
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+}
