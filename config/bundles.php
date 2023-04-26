3d6f97a3c9f9ab497bebb9c745c836a6573948e0
jeremybreant <breant.jeremy@yahoo.com>
2023-04-26 11:33:34 +0200

    adding dama_doctrine_test_bundle to reset db after a test + ûpgrade to phpunit 10

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
