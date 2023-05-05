fe41f6198af33d4ea8c449c4051ee5a49d0f56f4
jeremybreant <breant.jeremy@yahoo.com>
2023-05-05 10:45:31 +0200

    testing login + CRUD ingredient

---
+++ tests/Functional/LoginTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class LoginTest extends WebTestCase
+{
+    public function testIfLoginSuccessful(): void
+    {
+        $client = static::createClient();
+
+        // Get route by urlgenerator
+
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get("router");
+        $crawler = $client->request('GET', $urlGenerator->generate('security.login'));
+
+        // Form
+        $form = $crawler->filter("form[name=login]")->form([
+            "_username" => "admin@symrecipe.fr",
+            "_password" => "password"
+        ]);
+
+        $client->submit($form);
+
+        // Redirect + Home
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $client->followRedirect();
+        $this->assertRouteSame('home');
+    }
+
+    public function testIfLoginFailWhenPasswordIsWrong(): void
+    {
+        $client = static::createClient();
+
+        // Get route by urlgenerator
+
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get("router");
+        $crawler = $client->request('GET', $urlGenerator->generate('security.login'));
+
+        // Form
+        $form = $crawler->filter("form[name=login]")->form([
+            "_username" => "admin@symrecipe.fr",
+            "_password" => "password_"
+        ]);
+
+        $client->submit($form);
+
+        // Redirect + Home
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $client->followRedirect();
+        $this->assertRouteSame('security.login');
+        $this->assertSelectorTextContains("div.alert-danger", "Invalid credentials.");
+    }
+}
