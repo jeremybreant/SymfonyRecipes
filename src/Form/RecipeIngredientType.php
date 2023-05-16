76c598d344312243df635ad2a55f22bb24c1ec5c
jeremybreant <breant.jeremy@yahoo.com>
2023-05-16 13:44:18 +0200

    comment exception listener to get default error message + preparing element for recipe form

---
+++ src/Form/RecipeIngredientType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\RecipeIngredient;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipeIngredientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('quantity')
+            ->add('unitType')
+            ->add('ingredient')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => RecipeIngredient::class,
+        ]);
+    }
+}
