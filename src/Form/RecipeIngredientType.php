38f0bda9894bd8e69aaa2e3006960d91e8cf98a7
jeremybreant <breant.jeremy@yahoo.com>
2023-05-24 17:24:19 +0200

    choose only user's ingredient + node consistency

--- src/Form/RecipeIngredientType.php
+++ src/Form/RecipeIngredientType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Ingredient;
 use App\Entity\RecipeIngredient;
+use App\Repository\IngredientRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -11,11 +12,20 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class RecipeIngredientType extends AbstractType
 {
+    private $token;
+
+    public function __construct(TokenStorageInterface $token)
+    {
+        $this->token = $token;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
         $builder
             ->add('ingredient', EntityType::class, [
                 'class' => Ingredient::class,
@@ -25,7 +35,13 @@ class RecipeIngredientType extends AbstractType
                 'label' => 'Ingrédient :',
                 'label_attr' => [
                     'class' => 'form-label mt-4'
-                ]
+                ],
+                'query_builder' => function (IngredientRepository $ir) {
+                    return $ir->createQueryBuilder('i')
+                        ->where('i.user = :user')
+                        ->orderBy('i.name', 'ASC')
+                        ->setParameter('user' , $this->token->getToken()->getUser());
+                }
             ])
             ->add('quantity', NumberType::class,[
                 'attr' => [
