53b200d1eb7918a7867d6d8da15bdf576cee9b1a
jeremybreant <breant.jeremy@yahoo.com>
2023-10-10 15:01:57 +0200

    methods for contact controller

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'contact.index')]
+    #[Route('/contact', name: 'contact.index', methods:['GET','POST'])]
     public function index(
         Request $request,
         EntityManagerInterface $manager
